fd7c0738766ce1848133baae5e9f55c94cddd2c8
azeezolasunkanmi <azeezolasunkanmi99@yahoo.com>
2025-04-05 02:07:11 +0100

    removed the first video

--- src/components/Header/Background/index.jsx
+++ src/components/Header/Background/index.jsx
@@ -2,7 +2,6 @@ import PropTypes from "prop-types";
 import bg1 from "../../../assets/images/bg1.jpg";
 import bg2 from "../../../assets/images/bg2.avif";
 import bg3 from "../../../assets/images/bg3.jpg";
-import olavideo from "../../../assets/videos/OLAMIDE&RILIWANWEDDINGHIGHLIGHT.mp4";
 
 const Background = ({ playStatus, heroCount }) => {
   const css = "absolute inset-0 w-full h-full object-cover -z-50";
@@ -11,7 +10,7 @@ const Background = ({ playStatus, heroCount }) => {
     <>
       {playStatus ? (
         <video className={`${css} fade-in`} autoPlay muted loop>
-          <source src={olavideo} type="video/mp4" />
+          <source src="/OLAMIDE&RILIWANWEDDINGHIGHLIGHT.mp4" type="video/mp4" />
         </video>
       ) : (
         <img
